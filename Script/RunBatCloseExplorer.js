26a0eb33b58eb10ae2a5961449b79979e390e02e
Annia Jimenez <annia.jimenez@mycldqa.com>
2025-06-05 00:11:11 -0400

    Initial commit

---
+++ Script/RunBatCloseExplorer.js
@@ -0,0 +1,41 @@
+﻿function RunBatCloseExplorer() {
+  Project.Variables.project_path = Project.Path;
+  Project.Variables.upload_path = Project.Variables.project_path + "Stores\\Files\\closeExpExpl.bat";
+  Log.Message("Project Path is: " + Project.Variables.project_path);
+  Log.Message("Bat path is: " + Project.Variables.upload_path);
+  var shell = Sys.OleObject("WScript.Shell");
+  shell.Run('"' + Project.Variables.upload_path + '"', 0, true); 
+}
+
+function Hooks_OnStartTestCase(Sender, StartTestCaseParams)
+{
+  Project.Variables.project_path = Project.Path;
+
+  Project.Variables.upload_path = Project.Variables.project_path + "Stores\\Files\\closeExpExpl.bat";
+
+  Log.Message("Project Path is: " + Project.Variables.project_path);
+  Log.Message("Bat Folder is: " + Project.Variables.upload_path);
+
+  var shell = Sys.OleObject("WScript.Shell");
+  shell.Run('"' + Project.Variables.upload_path + '"', 0, true); 
+  
+ var browser = Browsers.Item(btChrome);
+  if (browser.Exists) {
+    Aliases.browser.BrowserWindow.Close();
+    Log.Message("Closed any existing Chrome instances.");
+  }
+ 
+  var options = "--disable-cache --clear-token-service --disable-application-cache --disable-session-crashed-bubble";
+ 
+  browser.RunOptions = options;
+  browser.Run();
+  Log.Message("Chrome launched with cache and cookies cleared.");
+ 
+  browser.Navigate("about:blank");
+  Log.Message("Navigated to 'about:blank' to complete the cache and cookies cleaning.");
+ 
+  Aliases.browser.BrowserWindow.Close();
+  Log.Message("Browser closed after cleaning cache and cookies.");
+  
+  
+}
---
+++ Script/VerifyUploadDocInDFPListView.js
@@ -0,0 +1,40 @@
+﻿function verifyListViewForMultipleTexts() {
+  var listView = Aliases.ExpExpl.dlg.page327702.ListView;
+  var targetTexts = [
+    "RoundPriceUpdated",
+    "TestingAttachments",
+    "Test1",
+    "Test2",
+    "Test"
+  ];
+
+  var foundTexts = [];
+  var missingTexts = [];
+
+  try {
+    if (listView.Exists) {
+      Log.Message("Checking for required list items...");
+
+      for (var i = 0; i < targetTexts.length; i++) {
+        var itemAlias = "Aliases.ExpExpl.dlg.page327702.ListView.listitem" + targetTexts[i];
+        
+        if (eval(itemAlias) && eval(itemAlias).Exists) {
+          foundTexts.push(targetTexts[i]);
+          Log.Checkpoint("Text found: '" + targetTexts[i] + "'");
+        } else {
+          missingTexts.push(targetTexts[i]);
+        }
+      }
+
+      if (missingTexts.length === 0) {
+        Log.Message("All required texts were found: " + foundTexts.join(", "));
+      } else {
+        Log.Error("The following texts were not found: " + missingTexts.join(", "));
+      }
+    } else {
+      Log.Error("The ListView object was not found.");
+    }
+  } catch (e) {
+    Log.Error("An error occurred while processing the ListView: " + e.message);
+  }
+}
